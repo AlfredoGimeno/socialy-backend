0937a5a98e2fe871d4df1aa3614df0ad29cd48de
Alfred <alfredogimeno13@gmail.com>
2026-04-20 11:04:08 +0200

    Proyecto back creado

---
+++ src/test/java/com/group/socialy/SocialyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group.socialy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
